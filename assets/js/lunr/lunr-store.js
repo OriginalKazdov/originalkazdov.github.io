fde518bbeddaaa419a2af727fdbed29e1ae6925b
OriginalKazdov <OriginalKazdov@users.noreply.github.com>
2025-07-08 01:01:44 +0000

    deploy: 64e750ba76c4614e187ab14a65d299e0b5293112

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Sobre mí",
+        "excerpt":"Hola, soy kazdov. Estoy formándome como pentester y uso este blog como bitácora técnica. Espero que puedan aprender algo de mis experiencias.  ","categories": [],
+        "tags": [],
+        "url": "/about/",
+        "teaser": null
+      },{
+        "title": "kazdov",
+        "excerpt":"Bienvenido a mi blog de ciberseguridad. Estoy estudiando para el eJPT y este espacio es donde documento mis prácticas, aprendizajes y desafíos reales.   Explorá mis entradas técnicas, experimentos y reflexiones sobre el camino del hacking ético.  ","categories": [],
+        "tags": [],
+        "url": "/",
+        "teaser": null
+      },{
+        "title": "Footprinting y escaneo activo con Nmap",
+        "excerpt":"Hoy estuve probando comandos de Nmap para hacer footprinting y escaneo activo. Primero usé: nmap -sn -PS 80 target Con esto buscaba ver si había un host activo usando el puerto 80. Después, cuando confirmé que había algo, escaneé puertos más interesantes como: nmap -p 21,22,445,8080,3389 target También encontré una...","categories": ["ciberseguridad","nmap"],
+        "tags": ["nmap","escaneo","footprinting"],
+        "url": "/ciberseguridad/nmap/2025/07/07/footprinting-y-escaneo-activo-con-nmap.html",
+        "teaser": null
+      }]
